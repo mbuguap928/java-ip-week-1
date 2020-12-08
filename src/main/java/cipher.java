ee075fb49fcc9c5b3c7b8e9ec2ab2be25e38cefd
mbuguap928 <mbuguap928@gmail.com>
2020-12-08 11:16:34 +0300

    modified gitignore file

--- src/main/java/cipher.java
+++ src/main/java/cipher.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public class cipher {
+public class cipher {
+
 }
--- src/test/java/cipherTest.java
+++ src/test/java/cipherTest.java
@@ -1,4 +1,11 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class cipherTest {
-  
+    @Test
+    public void inputAsString_inputValidation_boolean(){
+
+    }
+
 }
